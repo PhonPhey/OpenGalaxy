02f570ac537e67184ee95c5119fe2e36e38208f9
Phon Phey <phonphey@o2.pl>
2017-08-17 16:53:53 +0300

    Update driver.py
    special change drivers!!!!

--- driver.py
+++ driver.py
@@ -2,4 +2,4 @@ import os
 
 def execute_cqlsh(query):
     if query != "":
-        os.system("python2 /usr/local/bin/cqlsh --cqlversion=\"3.4.4\" -e \"%s\"" % query)
+        os.system("cqlsh -e \"%s\"" % query)
